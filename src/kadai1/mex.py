aa817847e1daf9a4829fd6e345c196df1037de90
SHINN314 <shinnosukekimura8@gmail.com>
2025-05-03 23:19:43 +0900

    add: ドキュメントを追加

--- src/kadai1/mex.py
+++ src/kadai1/mex.py
@@ -1,4 +1,17 @@
 def mex(T):
+  """
+  Calculate mex value.
+
+  Values
+  ----------
+  T: list
+  Finate subsete of N_0.
+
+  Returns
+  ----------
+  i: int
+  Mex.
+  """
   i = 0
 
   while((i in T)):
