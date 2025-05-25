198302fe67c2b86c276f8a11eeffdaba6e1e43b0
SHINN314 <shinnosukekimura8@gmail.com>
2025-05-25 22:55:35 +0900

    update: n×n用のchomp botを作った

--- src/kadai1/chomp_bot.py
+++ src/kadai1/chomp_bot.py
@@ -1,8 +1,9 @@
 from chomp import Chomp 
 
-class ChompBot:
+class SquareChompBot:
   """
-  This class is a bot for 2 times n chomp.
+  A bot which optimized for square chomp board.
+  This class is usefull if this bot is a first player of the geme
 
   Attributes
   ----------
@@ -12,7 +13,16 @@ class ChompBot:
   def __init__(self):
     pass
 
-  def delete_space(Chomp: Chomp, row: int, col: int):
+  def is_symmetric_board(chomp: Chomp) -> bool:
+    board = chomp.board
+    transposed_board = [list(x) for x in zip(*board)]
+
+    if board == transposed_board:
+      return True
+    
+    return False
+
+  def delete_space(self, chomp: Chomp):
     """
     Set the space on the board.
     
@@ -25,5 +35,29 @@ class ChompBot:
     col : int
         The column index of the space to set.
     """
-    Chomp.delete_space(row, col)
+    if self.is_symmetric_board(chomp):
+      # case square board
+      row_space = chomp.row-2
+      col_space = 1
+      chomp.delete_space(row=row_space, col=col_space)
+
+    else:
+      # case unsymmetrical l board
+      row_space = chomp.row-1
+      col_space = 0
+      board = chomp.board
+
+      for i in range(0, chomp.row):
+        if board[chomp.row-1-i][0] > board[chomp.row-1][i]:
+          # case if vertical edge is longer than horizon edge
+          chomp.delete_space(row=row_space, col=0)
+          break
+
+        elif board[chomp.row-1][i] > board[chomp.row-1-i][0]: 
+          # case if horizon edge is longer than vertical edge
+          chomp.delete_space(row=chomp.row-1, col=col_space)
+
+        else:
+          row_space += 1
+          col_space += 1
     
