1b1b157cdb01a97a7180e55dd35029bd61faccda
SHINN314 <shinnosukekimura8@gmail.com>
2025-06-05 23:13:32 +0900

    update: ニム積の関数名を変更

--- src/kadai1/nim_calc.py
+++ src/kadai1/nim_calc.py
@@ -5,7 +5,7 @@ memo = {}
 def nim_sum(a, b):
   return a ^ b
 
-def nim_mult_prime(a, b):
+def nim_time_prime(a, b):
   if(a == 0 and b == 0):
     return 0
   
@@ -13,15 +13,15 @@ def nim_mult_prime(a, b):
   
   for a_prime in range(0, a):
     for b_prime in range(0, b):
-      t = nim_sum(nim_mult_prime(a_prime, b), nim_mult_prime(a, b_prime))
-      t = nim_sum(t, nim_mult_prime(a_prime, b_prime))
+      t = nim_sum(nim_time_prime(a_prime, b), nim_time_prime(a, b_prime))
+      t = nim_sum(t, nim_time_prime(a_prime, b_prime))
 
       if(not(t in T)):
         T.append(t)
 
   return mex.mex(T)
 
-def nim_mult(a, b):
+def nim_time(a, b):
   result = 0
   while b:
     if b & 1:
