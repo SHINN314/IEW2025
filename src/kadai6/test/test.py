e0a481627306c2ba4d3612d7eb0239d010cbb1c4
SHINN314 <shinnosukekimura8@gmail.com>
2025-07-29 00:05:57 +0900

    chore: draw_vertical_edgesのテストコードを実装

--- src/kadai6/test/test.py
+++ src/kadai6/test/test.py
@@ -6,54 +6,16 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from adjacency_list import GameGraph
 import roopy_grundy
+import three_hold_nim_generator as nim_generator
 
 def main():
-    game_graph = GameGraph()
-    file_path = input("Enter the path to the game graph file: ")
-
-    # load file
-    print(f"----- load file -----")
+    x, y = map(int, input("Input x and y of grid: ").split())
     try:
-        game_graph.load_from_file(file_path)
-        print(f"✅ Game graph loaded successfully with {game_graph.total_nodes} nodes.")
+        vertical_next_node_set = nim_generator.draw_vertical_edges(x, y)
 
-    except FileNotFoundError:
-        print(f"❌ Error: File '{file_path}' not found.")
-    except ValueError as e:
-        print(f"❌ Error: Invalid file format - {e}")
+        print(f"grid ({x}, {y})'s next node: {vertical_next_node_set}")
     except Exception as e:
-        print(f"❌ Unexpected error: {e}")
-
-    # initialize Grundy numbers
-    print(f"----- initialize grundy numbers -----")
-    try:
-        grundy_numbers = roopy_grundy.initialize_grundy_numbers(game_graph)
-
-        # display initialized Grundy numbers
-        print("✅ Grundy numbers initialized:")
-        for node_id, grundy_number in enumerate(grundy_numbers):
-            print(f"Node {node_id}: Grundy number = {grundy_number}")
-
-    except Exception as e:
-        print(f"❌ Error initializing Grundy numbers: {e}")
-
-    # calculate m_n
-    print(f"----- calculate m_n -----")
-    try:
-        for node, _ in enumerate(game_graph.nodes):
-            m_n = roopy_grundy.m_n(game_graph, node, grundy_numbers)
-            print(f"Node: {node}: m_n = {m_n}")
-    except Exception as e:
-        print(f"Error calculating m_n: {e}")
-
-    # calculate grundy numbers
-    print(f"----- calculate grundy number -----")
-    try:
-        grundy_numbers = roopy_grundy.calculate_grundy_numbers(game_graph)
-        for node, grundy_number in enumerate(grundy_numbers):
-            print(f"Node {node}: grundy number = {grundy_number}")
-    except Exception as e:
-        print(f"Error calculating grundy number: {e}")
+        print(f"Drawing vertical next node set error: {e}")
 
 if __name__ == "__main__":
     main()
